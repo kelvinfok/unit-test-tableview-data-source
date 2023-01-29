70b2a37a405d301d2eb638e06a2a8a75be457c69
kelvinfok <punkd1@gmail.com>
2023-01-30 00:01:36 +0800

    Initial Commit

---
+++ tableview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableview
+//
+//  Created by Kelvin Fok on 30/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
